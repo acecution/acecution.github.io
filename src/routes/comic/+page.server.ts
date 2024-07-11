d6731e532916c9ece50ea98c31e8f8db4bc87bd1
acecution <acecution7@gmail.com>
2024-07-11 18:29:48 +0300

    fixed srr

--- src/routes/comic/+page.server.ts
+++ src/routes/comic/+page.server.ts
@@ -1,11 +1,15 @@
-import type { ComicApiResponse, ComicData } from '../../types.ts';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import type { ComicApiResponse, ComicData } from "../../types.ts";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 export async function load({ fetch }) {
-  const email : string = "a.varfolomeeva@innopolis.university";
-  const comicId : ComicApiResponse = await fetch(`https://fwd.innopolis.university/api/hw2?email=${email}`).then(r => r.json())
-  const comicData : ComicData = await fetch(`https://fwd.innopolis.university/api/comic?id=${comicId}`).then(r => r.json())
+  const email: string = "a.varfolomeeva@innopolis.university";
+  const comicId: ComicApiResponse = await fetch(
+    `https://fwd.innopolis.university/api/hw2?email=${email}`,
+  ).then((r) => r.json());
+  const comicData: ComicData = await fetch(
+    `https://fwd.innopolis.university/api/comic?id=${comicId}`,
+  ).then((r) => r.json());
 
   const { img, day, safe_title, year, month, alt } = comicData;
 
